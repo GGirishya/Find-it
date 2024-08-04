5a9eec343e807e272ab957b29a905a12fb74faa0
Guillaume Girishya <143455392+GGirishya@users.noreply.github.com>
2024-08-04 05:05:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Remove span children.
+let spans= document.querySelectorAll(".box span");
+console.log(spans);
+for(let i=0; i<spans.length, i++){
+  spans[i].remove();
+}
+// Create an image element.
+
+// Get a random box.
+
+// Make the new image element a child of the box.
